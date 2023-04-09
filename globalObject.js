929e27f62fa37fd3ad69c9b720ec9e62c837fa0b
v1singham <svikashkumar853@gmail.com>
2023-04-09 14:29:49 +0530

    module practic2e

---
+++ globalObject.js
@@ -0,0 +1,15 @@
+/*
+console , setTimeout,clearTimeout, setInterval etc are global object which is used in inside of browser , inside of client or inside of node or server.
+
+
+console.log('wnfj); js engine treat like window.console.log
+
+var message='kwjbfwf'
+log(global.message ) // undefined because of variables not added in global object
+
+variable and function defined in file not adde to global object, only scope to this file in which defilned.
+*/
+
+// var message='kwjbfwf'
+
+// console.log(global.message)
