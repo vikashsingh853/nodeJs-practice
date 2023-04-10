03727cf62cf9b53fe4473d1ae2591da8bf5ffce5
v1singham <svikashkumar853@gmail.com>
2023-04-10 09:56:44 +0530

    learning cotinue...

---
+++ package-manager/semanticVersioning.js
@@ -0,0 +1,36 @@
+// ^x.y.z // major.minor.patch version ^-> interest in major recetnversion. downloaded (4.y)
+// ~ tilde intterseted in any verion 1.x.x lataest version
+
+
+//  list the installed package (npm list)
+
+// view dependencies for package (npm view name)+/dependecies / versions
+
+// specific version of package npm i name@vesrion
+
+
+// upadte local version of package (npm outdated)
+
+// npm update  // update outdated version of package
+// npm i -g npm-check-updates -> npm-check-updates -> ncu -u //updates package.json -> npm i 
+
+
+
+// ----------------------------------------------------------------
+
+// devDependencies (dev dependencies not go to production)
+//  npm i jshint --save-dev 
+
+
+// ----------------------------------------------------------------
+// uninstall packages
+// npm un name
+
+
+// ----------------------------------------------------------------
+// gloabl package  (-g makes gloabl)
+// npm is gloabl package 
+// npm -g outdated
+
+// ----------------------------------------------------------------
+// publish own node package
