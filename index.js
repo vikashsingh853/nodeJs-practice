9fbcd99e544793f6ebc0c505113c857a76757fbe
v1singham <svikashkumar853@gmail.com>
2023-04-10 09:28:41 +0530

    learn package manager installation and ingnore files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-'kfneinieei'
+//  whenever use node then you have to package.json file 
+// npm init to create the package.json (--yes )
+
+//add node package
+  // npm install/i packagename or ...
+
+
+  var _=require('underscore');
+//assumes
+  // core module/ file or folder
+  // node_modules folder
+
+const res=_.contains([1,2,3,4,],4);
+
+console.log(res)
---
+++ package-manager/mangoose.js
@@ -0,0 +1,3 @@
+// install mangoose
+// npm i mongoose
+// Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports Node.js and Deno (alpha)
